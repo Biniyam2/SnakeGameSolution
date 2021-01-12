745501e01fb1c8a0937850dfd72a6859d10af210
Biniyam2 <75283051+Biniyam2@users.noreply.github.com>
2021-01-12 11:18:48 -0500

    Add project files.

---
+++ SnakeGame/SnakeGame.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace SnakeGame
+{
+    partial class SnakeGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.landArea = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.pointLabel = new System.Windows.Forms.Label();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newSnakeGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.signInButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // landArea
+            // 
+            this.landArea.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.landArea.Location = new System.Drawing.Point(-1, 78);
+            this.landArea.Name = "landArea";
+            this.landArea.Size = new System.Drawing.Size(402, 341);
+            this.landArea.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(428, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(12, 25);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(71, 24);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "Score:";
+            // 
+            // pointLabel
+            // 
+            this.pointLabel.AutoSize = true;
+            this.pointLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pointLabel.Location = new System.Drawing.Point(78, 25);
+            this.pointLabel.Name = "pointLabel";
+            this.pointLabel.Size = new System.Drawing.Size(21, 24);
+            this.pointLabel.TabIndex = 3;
+            this.pointLabel.Text = "0";
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseButton.Location = new System.Drawing.Point(546, 361);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(78, 33);
+            this.pauseButton.TabIndex = 4;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pausetButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(651, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newSnakeGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // newSnakeGameToolStripMenuItem
+            // 
+            this.newSnakeGameToolStripMenuItem.Name = "newSnakeGameToolStripMenuItem";
+            this.newSnakeGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newSnakeGameToolStripMenuItem.Text = "New Snake Game";
+            this.newSnakeGameToolStripMenuItem.Click += new System.EventHandler(this.newSnakeGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(492, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Player";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(440, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 35);
+            this.textBox1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(407, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(232, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // signInButton
+            // 
+            this.signInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signInButton.Location = new System.Drawing.Point(485, 147);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(78, 33);
+            this.signInButton.TabIndex = 9;
+            this.signInButton.Text = "Sign in";
+            this.signInButton.UseVisualStyleBackColor = true;
+            this.signInButton.Click += new System.EventHandler(this.signInButton_Click);
+            // 
+            // SnakeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 418);
+            this.Controls.Add(this.signInButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.pointLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.landArea);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SnakeGame";
+            this.Text = "Snake Game";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel landArea;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label pointLabel;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newSnakeGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button signInButton;
+    }
+}
+
---
+++ SnakeGame/SnakeGame.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class SnakeGame : Form
+    {
+        //static int xPosition = 120;  //359, 274
+        //static int yPosition = 120;
+        PictureBox[] snakeParts;
+        static int snakeSize = 5;
+        Point location = new Point(120,120);
+        PictureBox apple = new PictureBox();
+        Point appleLocation = new Point();
+        string userName = "";
+        string direction = "right";
+        bool changingDirection = false;
+        bool snakeTuchApple = true;
+        bool isAppleEaten = false;
+        int ScoreCount = 0;
+        DataTable playerTable = new DataTable();
+        DataTable ScoreTable = new DataTable();
+
+        public SnakeGame()
+        {
+            InitializeComponent();
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+        public void NewGame()
+        {
+            landArea.Controls.Clear();
+            snakeParts = null;
+            snakeSize = 5;
+            snakeTuchApple = true;
+            ScoreCount = 0;
+            pointLabel.Text = ScoreCount.ToString();
+            location = new Point(120, 120);
+            drowSnake();
+            appleDrow();
+            timer1.Start();
+            Button button = new Button();
+            foreach (Control c in Controls)
+            {
+                button = c as Button;
+                if (button != null)
+                {
+                    button.Enabled = true;
+                }
+            }
+        }
+        private void pausetButton_Click(object sender, EventArgs e)
+        {
+            if(pauseButton.Text == "Pause")
+            {
+                timer1.Enabled = false;
+                pauseButton.Text = "Continue";
+            }
+            else
+            {
+                pauseButton.Text = "Pause";
+                timer1.Enabled = true;
+            }
+        }
+        //Apple Snake
+        public void drowSnake()
+        {    
+            snakeParts = new PictureBox[snakeSize];
+         for(int i = 0; i< snakeSize; i++)
+         {
+            snakeParts[i] = new PictureBox();
+            snakeParts[i].Size = new Size(10,10);
+            snakeParts[i].BackColor = Color.Green;
+            snakeParts[i].BorderStyle = BorderStyle.FixedSingle;
+            snakeParts[i].Location = new Point(location.X - (10 * i), location.Y);
+            landArea.Controls.Add(snakeParts[i]);
+         }
+        }
+        //Apple drow
+        public void appleDrow()
+        {
+            Random position = new Random();
+            int appleXDim = position.Next(38) * 10;
+            int appleYDim = position.Next(30) * 10;
+             
+                if (snakeParts[0].Location == new Point(appleXDim, appleYDim))
+                {
+                   appleXDim = position.Next(38) * 10;
+                   appleYDim = position.Next(38) * 10;
+                }
+
+            if(snakeTuchApple == true)
+            {
+                appleLocation = new Point(appleXDim, appleYDim);
+                apple.Size = new Size(10,10);
+                apple.BackColor = Color.Red;
+                apple.BorderStyle = BorderStyle.FixedSingle;
+                apple.Location = appleLocation;
+                landArea.Controls.Add(apple);
+                snakeTuchApple = false;
+            }
+        }
+        //Drow wall
+        public void hitItSelf()
+        {
+            for(int i =1; i<snakeSize; i++)
+            {
+                if (snakeParts[0].Location == snakeParts[i].Location)
+                {
+                    GameOver();
+                }
+            }
+        }
+        public void hitWall()
+        {
+          if(snakeParts[0].Location.X < 0 || snakeParts[0].Location.X > 402 || snakeParts[0].Location.Y < 0 || snakeParts[0].Location.Y > 341)
+          {
+                GameOver();
+          }
+      
+        }
+        public void GameOver() 
+        {
+            timer1.Enabled = false;
+            Button button = new Button();
+            foreach(Control c in Controls)
+            {
+                button = c as Button;
+                if (button != null)
+                {
+                    button.Enabled = false;
+                }
+            }
+
+        }
+        //Move Snake 
+        public void snakeMovement()
+        {
+            Point move = new Point(0, 0);
+            for (int i = 0; i < snakeSize; i++)
+            {
+                
+                if (i == 0)
+                {
+                    move = snakeParts[i].Location;
+                    if (direction == "up")
+                    {
+                        snakeParts[0].Location = new Point(snakeParts[0].Location.X, snakeParts[0].Location.Y - 10);
+                    }
+                    if (direction == "down")
+                    {
+                        snakeParts[0].Location = new Point(snakeParts[0].Location.X, snakeParts[0].Location.Y + 10);
+                    }
+                    if (direction == "right" )
+                    {
+                        snakeParts[0].Location = new Point(snakeParts[0].Location.X + 10, snakeParts[0].Location.Y);
+                    }
+                    if (direction == "left" )
+                    {
+                        snakeParts[0].Location = new Point(snakeParts[0].Location.X - 10, snakeParts[0].Location.Y);
+                    }
+                }
+                else
+                {
+                    Point newMove = snakeParts[i].Location;
+                    snakeParts[i].Location = move;
+                    move = newMove;
+                }
+                
+            }
+            changingDirection = false;
+        }
+        public void EatApple()
+        {
+    
+            if (snakeParts[0].Location == appleLocation)
+            {
+                isAppleEaten = true;
+                snakeSize++;
+                PictureBox[] oldSnake = snakeParts;
+                landArea.Controls.Clear();
+                snakeParts = new PictureBox[snakeSize];
+                for(int i = 0; i<snakeSize; i++)
+                {
+                    snakeParts[i] = new PictureBox();
+                    snakeParts[i].Size = new Size(10,10);
+                    snakeParts[i].BackColor = Color.Green;
+                    snakeParts[i].BorderStyle = BorderStyle.FixedSingle;
+                    if (i == 0)
+                        snakeParts[i].Location = appleLocation;
+                    else
+                        snakeParts[i].Location = oldSnake[i - 1].Location;
+                    landArea.Controls.Add(snakeParts[i]);
+                }
+                ScoreCount++;
+                if(timer1.Interval !=50)
+                    timer1.Interval -= 20;
+                pointLabel.Text = ScoreCount.ToString();
+                isAppleEaten = false;
+                snakeTuchApple = true;
+                appleDrow();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            hitItSelf();
+            hitWall();
+            EatApple();
+            snakeMovement();    
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+           
+            if (keyData == Keys.Up && changingDirection != true && direction != "down")
+            {
+                direction = "up";
+                changingDirection = true;
+            }
+            if (keyData == Keys.Down && changingDirection != true && direction != "up")
+            {
+                direction = "down";
+                changingDirection = true;
+            }
+            if (keyData == Keys.Left && changingDirection != true && direction != "right")
+            {
+                direction = "left";
+                changingDirection = true;
+            }
+            if (keyData == Keys.Right && changingDirection != true && direction != "left")
+            {
+                direction = "right";
+                changingDirection = true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by Biniyam Yemane");
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void newSnakeGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+        private void signInButton_Click(object sender, EventArgs e)
+        {
+           userName = textBox1.Text;    
+        }
+        public void DataSource()
+        {
+            SqlConnection myCon = new SqlConnection(@"Data Source = (local)\SQLEXPRESS; initial catalog = GamesDataBase;
+                                                                                        integrated security = True;");
+            SqlDataAdapter myDA = new SqlDataAdapter("select * from Score", myCon);
+            myDA.Fill(ScoreTable);
+            dataGridView1.DataSource = ScoreTable;
+            myCon.Close();
+
+            SqlCommand myCommand = new SqlCommand();
+            myCommand.Connection = myCon;
+            myCommand.CommandText = @"insert into Score"
+                + " (User_Name, RateChangeDate, shiftID, rate, payfrequency, numberss)"
+                + "Values (@employeeID, @RateChangeDate, @shiftID, @rate, @payfrequency, @numberss)";
+
+
+        }
+    }
+}
